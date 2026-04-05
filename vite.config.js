da8806dc46446e65ed75358845069d91c049f054
Thilina Dhanajaya <thilina.karunarathne@zettab.io>
2026-04-05 11:52:10 +0530

    Revert "Add GitHub Pages deployment"
    This reverts commit 79bdc97d2f0d29cc64fd5d0b34cdf5723f7007a1.

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/Bimsara-web/',
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
