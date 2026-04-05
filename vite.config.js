6067fd196b16e86e6ef4ddf3f6c699dfb246e290
Thilina Dhanajaya <thilina.karunarathne@zettab.io>
2026-04-05 11:51:56 +0530

    Revert "build 1"
    This reverts commit 82d9a64ee05edb63e5c1f288cfd1c5f42aca5aba.

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/bimsara-web/',
+  base: '/Bimsara-web/',
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
