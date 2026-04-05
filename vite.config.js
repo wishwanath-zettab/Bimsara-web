8a8f22c49a609ee0adf9600c305da63196974c9e
Thilina Dhanajaya <thilina.karunarathne@zettab.io>
2026-04-05 12:12:17 +0530

    bild fix

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/bimsara-web/' : '/',
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
@@ -16,4 +17,4 @@ export default defineConfig({
       },
     },
   },
-})
+}))
