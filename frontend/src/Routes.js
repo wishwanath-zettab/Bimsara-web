2918816a61b6887b3579f9fdbf2f7f3b6f3d4a43
Lakmal L Abeywickrama <114899787+abevicrama@users.noreply.github.com>
2026-04-29 15:31:24 +0530

    now working admin panel on :port/admin

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -6,6 +6,8 @@ import Buyers from "./screens/serviceScreen/buyers/buyers";
 import Landlords from "./screens/serviceScreen/landlords/landloards";
 import Sellers from "./screens/serviceScreen/sellers/sellers";
 import Tenants from "./screens/serviceScreen/tenants/tenants";
+import AdminLogin from "./pages/admin/AdminLogin";
+import AdminDashboard from "./pages/admin/AdminDashboard";
 
 const Routers = () => {
   return (
@@ -17,6 +19,8 @@ const Routers = () => {
         <Route exact path="/buyers" element={<Buyers />}></Route>
         <Route exact path="/landlords" element={<Landlords />}></Route>
         <Route exact path="/tenants" element={<Tenants />}></Route>
+        <Route path="/admin" element={<AdminLogin />} />
+        <Route path="/admin/dashboard" element={<AdminDashboard />} />
       </Routes>
     </BrowserRouter>
   );
