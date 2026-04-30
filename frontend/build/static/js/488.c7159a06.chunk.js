a53f338efbe587f7996ba29f1329134a5020b914
Lakmal L Abeywickrama <114899787+abevicrama@users.noreply.github.com>
2026-04-30 15:51:16 +0530

    remove links

--- frontend/build/static/js/488.c7159a06.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(globalThis.webpackChunkbimsara_frontend=globalThis.webpackChunkbimsara_frontend||[]).push([[488],{2488(a,e,n){new WeakMap;new Set}}]);
-//# sourceMappingURL=488.c7159a06.chunk.js.map
